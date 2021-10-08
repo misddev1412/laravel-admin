c80604f6b95a119029b74606ce20c7dc16f7ae41
Thinh <misddev@gmail.com>
2021-10-08 23:35:47 +0700

    no message

--- resources/js/components/Crud/Fields/Wysiwyg/FieldWysiwyg.vue
+++ resources/js/components/Crud/Fields/Wysiwyg/FieldWysiwyg.vue
@@ -113,17 +113,7 @@ export default {
             editorData: '',
             editorContent: ClassicEditor,
             editorConfig: {
-              
-                plugins: [ CKFinder],
-                ckfinder: {
-                    // Upload the images to the server using the CKFinder QuickUpload command.
-                    uploadUrl: 'https://example.com/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images&responseType=json',
-
-                    // Define the CKFinder configuration (if necessary).
-                    options: {
-                        resourceType: 'Images'
-                    }
-                },
+           
 
             }
 
