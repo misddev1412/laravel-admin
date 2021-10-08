a6cc59c6bc18de410410daa618a9f43d5324b413
Thinh <misddev@gmail.com>
2021-10-08 23:10:33 +0700

    no message

--- resources/js/components/Crud/Fields/Wysiwyg/FieldWysiwyg.vue
+++ resources/js/components/Crud/Fields/Wysiwyg/FieldWysiwyg.vue
@@ -119,21 +119,7 @@ export default {
             editorData: '',
             editor: ClassicEditor,
             editorConfig: {
-                toolbarGroups : [
-                    { name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
-                    { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
-                    { name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
-                    { name: 'forms', groups: [ 'forms' ] },
-                    { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-                    { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
-                    { name: 'links', groups: [ 'links' ] },
-                    { name: 'insert', groups: [ 'insert' ] },
-                    { name: 'styles', groups: [ 'styles' ] },
-                    { name: 'colors', groups: [ 'colors' ] },
-                    { name: 'tools', groups: [ 'tools' ] },
-                    { name: 'others', groups: [ 'others' ] },
-                    { name: 'about', groups: [ 'about' ] }
-                ],
+              
                 plugins: [ CKFinder],
                 ckfinder: {
                     // Upload the images to the server using the CKFinder QuickUpload command.
@@ -145,7 +131,6 @@ export default {
                     }
                 },
 
-                removeButtons: 'NewPage,Print,Save,Templates,Replace,Find,SelectAll,Scayt,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,CreateDiv,Anchor,Flash,Smiley,PageBreak,ShowBlocks,About,Language'
             }
 
         };
