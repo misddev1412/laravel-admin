bda756dad3aa3b8050e3616f80c14a93102f600a
Thinh <misddev@gmail.com>
2021-10-08 23:45:18 +0700

    no message

--- resources/js/components/Crud/Fields/Wysiwyg/FieldWysiwyg.vue
+++ resources/js/components/Crud/Fields/Wysiwyg/FieldWysiwyg.vue
@@ -44,7 +44,7 @@
 import Vue from 'vue';
 
 import CKEditor from '@ckeditor/ckeditor5-vue2';
-import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
+// import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 
 Vue.use( CKEditor );
 // import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
